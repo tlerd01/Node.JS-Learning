65db1ed425c889b264d1839a1fa01b070c22fd11
tlerd01 <50984872+tlerd01@users.noreply.github.com>
2022-11-01 16:33:47 +0700

    Up

--- app.js
+++ app.js
@@ -5,18 +5,21 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 4000;
 
-app.use(morgan('combined'))
-app.use(express.static(path.join(__dirname,"/public/")))
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
+
+app.set("views","./src/views");
+app.set("view engine", "ejs")
 
 app.get("/", (req,res) =>{
 
-    res.send('Hello Teerapong Handsome');
+    res.render('index',{username: 'TeeZa55+', customers: ['Kitti','Kittikorn','Kitty']});
 
 })
 
-app.listen(port, ()=>{
+app.listen(PORT, ()=>{
 
-    debug("Listening on port" + chalk.red(port));
+    debug("Listening on port" + chalk.red(PORT));
 })

1da8aa86e74075941166bef190e7eb773a1c6003
tlerd01 <50984872+tlerd01@users.noreply.github.com>
2022-11-01 17:00:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,23 +9,23 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"/public/")));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.set("views","./src/views");
-app.set("view engine", "ejs")
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
 
-productRouter.route("/").get((req,res) =>{
-    res.send("Hello World !! I'm Product")
+productRouter.route("/").get((req, res) => {
+    res.send("Hello World !! I'm Product");
+});
+app.use("/products", productRouter);
+
+app.get("/", (req, res) => {
+
+    res.render('index', { username: 'TeeZa55+', customers: ['Kitti', 'Kittikorn', 'Kitty'] });
+
 })
-app.use("/products", productRouter)
 
-app.get("/", (req,res) =>{
-
-    res.render('index',{username: 'TeeZa55+', customers: ['Kitti','Kittikorn','Kitty']});
-
-})
-
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
 
     debug("Listening on port" + chalk.red(PORT));
 })
